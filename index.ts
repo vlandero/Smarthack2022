de1a76c22ecefcd939d465383aa3ce694f14eaca
Vlad-Andrei ROSU <Vlad.Rosu.ext@cycleon.com>
2022-11-04 00:14:27 +0200

    finish restart

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ app.use(cors({
 }));
 
 app.get("/",(req:Request,res:Response) => {
-    res.send("Hello there new");
+    res.send("Hello there newwwww");
 })
 
 app.get("/x",(req:Request,res:Response) => {
