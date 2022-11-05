257e1a1aeef907c4ee9f3bdb4f0a923e98b72f6b
Vlad-Andrei ROSU <Vlad.Rosu.ext@cycleon.com>
2022-11-05 19:11:51 +0200

    add models

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,38 @@ const cors = require('cors');
 const path = require('path')
 import { Request,Response } from "express";
 import * as mongoose from "mongoose";
+import { ownerSchema, startupSchema } from "../models/mongo-models";
 require('dotenv').config();
 
 
 const app = express();
-app.use(express.static(path.resolve(__dirname, './fe/build')));
+app.use(express.static(path.resolve(__dirname, '../fe/build')));
 app.use(express.json());
 app.use(cors({
     origin: '*'
 }));
 
 mongoose.connect(process.env.MONGO_URI);
+// const Startupp = mongoose.model('Startup', startupSchema);
 
-app.get("/x",(req:Request,res:Response) => {
+// const startup = new Startupp({
+//     name: "Startup 1",
+//     employees: [],
+//     menus: [{name: "Menu 1", items: []}],
+// })
+
+app.get("/x",async (req:Request,res:Response) => {
+    // await startup.save()
     res.send({x:"Hello world"});
 })
 
 app.post("/api",(req:Request,res:Response) => {
     console.log(req.body);
-    res.status(400).send('Hello');
+    res.status(200).send(req.body);
 })
 
 app.get('*', (req:Request, res:Response) => {
-    res.sendFile(path.resolve(__dirname, './fe/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../fe/build', 'index.html'));
 });
 
 app.listen(1234, () => {
