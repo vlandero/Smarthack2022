f91ff99f3ef8614ecd7530c5cd15ab04ccf2cab8
andrei2002f <farcasiu.andrei@gmail.com>
2022-11-05 19:32:15 +0200

    add react

---
+++ fe/src/Components/LandingPage.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import React from "react";
+import "../App.css";
+
+const LandingPage = () => {
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/logIn`;
+    navigate(path);
+  };
+  const routeChange2 = () => {
+    let path = `/signUp`;
+    navigate(path);
+  };
+
+  return (
+    <div className="landingPage">
+      <h1>Dream Startup</h1>
+      <h3>qwbvrnwqnbreqn</h3>
+      <button onClick={routeChange}>Log in</button>
+      <button onClick={routeChange2}>Sign up</button>
+    </div>
+  );
+};
+
+export default LandingPage;
